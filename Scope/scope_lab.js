88029d99aa5b84427c6f7d6513befe8b6dd56c8d
maharajsandip <maharaj_sandip@berkeley.edu>
2025-02-15 10:28:55 -0800

    Control flow lab

--- Scope/scope_lab.js
+++ Scope/scope_lab.js
@@ -1,33 +1,46 @@
-// Global scope
-var globalVar = "I'm a global variable";
-let globalLet = "I'm also global, but scoped with let";
-const globalConst = "I'm a global constant";
+// // Global scope
+// var globalVar = "I'm a global variable";
+// let globalLet = "I'm also global, but scoped with let";
+// const globalConst = "I'm a global constant";
 
 
-{
-// Block scope
-var blockVar = "I'm a block-scoped var";
-let blockLet = "I'm a block-scoped let";
-const blockConst = "I'm a block-scoped const";
-}
+// {
+// // Block scope
+// var blockVar = "I'm a block-scoped var";
+// let blockLet = "I'm a block-scoped let";
+// const blockConst = "I'm a block-scoped const";
+// }
 
-// Global scope
-console.log(globalVar); // Output: "I'm a global variable"
-console.log(globalLet); // Output: "I'm also global, but scoped with let"
-console.log(globalConst); // Output: "I'm a global constant"
+// // Global scope
+// console.log(globalVar); // Output: "I'm a global variable"
+// console.log(globalLet); // Output: "I'm also global, but scoped with let"
+// console.log(globalConst); // Output: "I'm a global constant"
 
-//Block Scope
-// console.log(blockVar);
-// console.log(blockLet);
+// //Block Scope
+// // console.log(blockVar);
+// // console.log(blockLet);
 
 function show(){
 var functionVar = "I'm a block-scoped var";
 let functionLet = "I'm a block-scoped let";
 const functionConst = "I'm a block-scoped const";
+// console.log(functionConst);
 }
 show();
 
-console.log(functionVar); // Throws ReferenceError
-console.log(functionLet); // Throws ReferenceError
-console.log(functionConst); // Throws ReferenceError
+// console.log(functionVar); // Throws ReferenceError
+// console.log(functionLet); // Throws ReferenceError
+// console.log(functionConst); // Throws ReferenceError
 
+{
+    let a = 'aa'; // block scope
+    var b = 'bb';
+    const c = 'cb';
+    // console.log(a);
+    // console.log(b);
+    // console.log(c);
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c); // block scope
